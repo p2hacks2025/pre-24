44ac4b908e4c288788c3487fefc6fac679ed3988
Aoi <test@example.com>
2025-12-20 05:24:58 +0900

    コメント機能の追加

--- hello-api/index.js
+++ hello-api/index.js
@@ -30,7 +30,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// ★ これが無いとHTMLが表示されない
+//これが無いとHTMLが表示されない
 app.use(express.static(
   path.join(__dirname, "../frontend")
 ));
@@ -42,12 +42,16 @@ let nextStarSignID = 1;
 const users = [];
 let nextUserID = 1;
 
+const albumComments = [];
+
 /* ===== API ===== */
 
-/*地図ピン
-牡羊座（４か所）：五稜郭タワー、キャンバス、千代台公園、昭和公園
-かに座（５か所）：函館駅、トランジスタカフェ、函館公園、函館八幡宮、住三吉神社
-*/
+/* マップ */
+
+// 牡羊座（４か所）：五稜郭タワー、キャンバス、千代台公園、昭和公園
+// かに座（５か所）：函館駅、トランジスタカフェ、函館公園、函館八幡宮、住三吉神社
+
+//　座標を返す
 const mapPins = {
   aries:{ //牡羊座
     stars: [
@@ -80,6 +84,7 @@ const mapPins = {
   }
 };
 
+//　星座ごとのマップ情報を取得
 app.get("/map/:starsign", (req,res) => {
   const {starsign} = req.params;
 
@@ -92,14 +97,12 @@ app.get("/map/:starsign", (req,res) => {
   res.json(pins);
 });
 
-// 動作確認
+//　動作確認
 app.get("/hello", (req, res) => {
   res.send("Hello World");
 });
 
-
-
-// 星座ごとの画像一覧取得
+//　星座ごとの画像一覧取得
 app.get("/images", (req, res) => {
   const star = req.query.star;
   if (!star) {
@@ -109,12 +112,12 @@ app.get("/images", (req, res) => {
   const dir = path.join(__dirname, "uploads", star);
 
   if (!fs.existsSync(dir)) {
-    return res.json([]); // フォルダがなければ空
+    return res.json([]); //フォルダがなければ空
   }
 
   const files = fs.readdirSync(dir);
 
-  // 画像URLに変換
+  //画像URLに変換
   const imageUrls = files.map(file =>
     `/uploads/${star}/${file}`
   );
@@ -122,7 +125,7 @@ app.get("/images", (req, res) => {
   res.json(imageUrls);
 });
 
-// Google Maps APIキー
+//　Google Maps
 app.get("/maps", (req, res) => {
   console.log("ENV:", process.env.GOOGLE_MAPS_API_KEY);
   res.json({
@@ -130,7 +133,8 @@ app.get("/maps", (req, res) => {
   });
 });
 
-// ユーザー登録
+/* ユーザー情報 */
+// ユーザー
 app.post("/users", (req, res) => {
   const {username, email, password} = req.body;
 
@@ -168,7 +172,7 @@ app.post("/login", (req, res) => {
   });
 });
 
-// 星座保存
+//　星座保存
 app.post("/stars", (req, res) => {
   const {user_id, star_name, start_date, end_date} = req.body;
 
@@ -188,11 +192,13 @@ app.post("/stars", (req, res) => {
   res.json(starSigns);
 });
 
+/* アルバム */
+
 //写真保存
 app.post("/upload", upload.single("photo"), (req, res) => {
   console.log("upload API called");
 
-  //★ 動作確認用
+  //動作確認用
   console.log("query.star =", req.query.star);
   console.log("body.star =", req.body.star);
   console.log(req.file);
@@ -213,12 +219,12 @@ app.post("/upload", upload.single("photo"), (req, res) => {
   });
 });
 
-// 星座ごとの写真一覧取得
+//星座ごとの写真一覧取得
 app.get("/photos/:star", (req, res) => {
   const star = req.params.star;
   const dir = path.join(__dirname, "uploads", star);
 
- // フォルダがなければ作成
+ //フォルダがなければ作成
  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
  
 
@@ -229,6 +235,43 @@ app.get("/photos/:star", (req, res) => {
   res.json(files);
 });
 
+// コメント取得（表示用）
+app.get("/albums/:star/comment", (req, res) => {
+  const {star} = req.params;
+
+  const comment  = albumComments.find(c => c.star === star);
+
+  if(!comment){
+    return res.json({content: ""});
+  }
+
+  res.json({
+    content: comment.content,
+    updated_at: comment.updated_at
+  });
+});
+
+// コメント保存
+app.put("/albums/:star/comment", (req, res) => {
+  const {star} = req.params;
+  const {content} = req.body;
+
+  const existing = albumComments.find(c => c.star === star);
+
+  if(existing){
+    existing.content = conntent ?? "";
+    existing.updated_at = new Date().toISOString();
+  }
+  else{
+    albumComments.push({
+      star,
+      content: content ?? "",
+      updated_at: new Date().toISOString()
+    });
+  }
+
+  res.json({message: "コメントを保存しました"})
+})
 
 /* ===== 起動 ===== */
 
